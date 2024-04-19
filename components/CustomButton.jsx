3e77def621d777288faf56762271efca785cfca2
christoseteoglou <itsmeglue@gmail.com>
2024-04-19 12:15:33 +0200

    added customButton and sign in page, with form field

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,4 +1,5 @@
-import { ActivityIndicator, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import React from "react";
 
 const CustomButton = ({
   title,
@@ -11,7 +12,7 @@ const CustomButton = ({
     <TouchableOpacity
       onPress={handlePress}
       activeOpacity={0.7}
-      className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} ${
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
         isLoading ? "opacity-50" : ""
       }`}
       disabled={isLoading}
@@ -19,15 +20,6 @@ const CustomButton = ({
       <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
         {title}
       </Text>
-
-      {isLoading && (
-        <ActivityIndicator
-          animating={isLoading}
-          color="#fff"
-          size="small"
-          className="ml-2"
-        />
-      )}
     </TouchableOpacity>
   );
 };
